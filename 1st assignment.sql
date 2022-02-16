fc862bb2c714db52da205378ec178a690cae690e
Rafal <rafalpierscieniak20@gmail.com>
2022-02-16 12:49:24 +0100

    query 8-10

--- 1st assignment.sql
+++ 1st assignment.sql
@@ -235,15 +235,50 @@ alter table "categoriesToGenresLookup"
 
 -- Question 4
 
-    -- modifying data #1
+-- Trigger when book has been sold, update its quantity in stock
 
-    -- modifying data #2
+    CREATE or REPLACE FUNCTION sellBook() RETURNS trigger AS $sell_book$
+        BEGIN
+        UPDATE books SET quantity = quantity - new.quantity WHERE pk = new."FK_book";
+        RETURN new;
+        END;
+    $sell_book$ LANGUAGE plpgsql;
 
-    -- modifying data #3
+    CREATE TRIGGER bookQuantityAfterOrderLookup AFTER INSERT ON "booksToOrdersLookup" FOR EACH ROW
+    EXECUTE PROCEDURE sellBook();
 
-    -- modifying data #4
+    -- 1. Sell a book to the customer
+    insert into orders ("FK_Customer", "dateTime")
+        values (3, now());
 
-    -- modifying data #5
+    insert into "booksToOrdersLookup" ("FK_order", "FK_book", quantity)
+        values (8,3,1);
+
+    -- 2. Change the address of the customer
+    UPDATE customers SET address = 'New address street 66'
+        WHERE customers.pk = 1;
+
+    -- 3. Add an existing author to a book
+    INSERT INTO "booksToAuthorsLookup" ("FK_book", "FK_author") VALUES (1, 5);
+
+    -- 4. Retire the "Space Opera" category and assign all books from that category to the parent category. Do not assume you know an id of the parent category
+
+    UPDATE "booksToCategoriesLookup" set "FK_category" =
+        (SELECT "FK_categoryParent" from "categoriesLookup" where "categoriesLookup"."FK_categoryChild" =
+                                                                  (SELECT pk from categories where categories.name = 'Space Opera'))
+    WHERE "booksToCategoriesLookup"."FK_category" =
+          (SELECT pk from categories where categories.name = 'Space Opera');
+
+    DELETE FROM "categoriesLookup" WHERE "FK_categoryChild"= (SELECT pk from categories where categories.name = 'Space Opera');
+    DELETE FROM categories WHERE name = 'Space Opera';
+
+    -- 5. Sell 3 copies of one book and 2 of another in a single order
+    insert into orders ("FK_Customer", "dateTime")
+        values (6, now());
+
+    insert into "booksToOrdersLookup" ("FK_order", "FK_book", quantity)
+        values (9,3,3),(9,2,2);
+
 
     -- querying data #1 (all books for an author)
     SELECT
@@ -282,7 +317,27 @@ alter table "categoriesToGenresLookup"
     select * from categories inner join "categoriesToGenresLookup" cTGL on categories.pk = cTGL."FK_category"
     inner join genres g on cTGL."FK_genre" = g.pk where  g.pk = null;
 
-    -- querying data #7 (ISBN number of books with more than one author)
+-- !!!!!!!!!!!!!!!!!!!! FIXME: does not work properly , -- querying data #7 (ISBN number of books with more than one author)
     select  public.books.isbn from books inner join "booksToAuthorsLookup" on books.pk = "booksToAuthorsLookup"."FK_book"
     inner join authors a on "booksToAuthorsLookup"."FK_author" = a.pk where books.pk = a.pk;
+
+    -- querying data #8 (ISBN numbers of books that sold at least X copies (you decide the value for X) )
+        -- let x denote 10 copies
+    select isbn from books inner join
+            (select bTOL."FK_book", sum(bTOL.quantity) as totalSold from "booksToOrdersLookup" bTOL group by "FK_book") as booksSold
+    on pk = booksSold."FK_book" where booksSold.totalSold > 10;
+
+    -- querying data #9 (Number of copies of each book sold – unsold books should show as 0 sold copies)
+    select pk, title, coalesce(totalSold,0) as sold from
+        (select pk, books.title, sum(bTOL.quantity)as totalSold from "booksToOrdersLookup" bTOL
+            full outer join books on  bTOL."FK_book" = books.pk group by "FK_book", title, pk) as pttS;
+
+    -- querying data #10 ( Best-selling books: The top 10 selling books ordered in descending order by number of sales. )
+        -- as we have just 10 book, we will query top 5 selling books
+
+    select pk, title, coalesce(totalSold,0) as sold from
+        (select pk, books.title, sum(bTOL.quantity)as totalSold from "booksToOrdersLookup" bTOL
+            full outer join books on  bTOL."FK_book" = books.pk group by "FK_book", title, pk) as pttS
+    order by sold DESC limit 5;
+    
 
